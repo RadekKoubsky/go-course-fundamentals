d182bc961d424da612186af19182e532be7c1746
Radek Koubsky <radekkoubsky@gmail.com>
2021-11-25 22:26:49 +0100

    Channels part 2

---
+++ section22_channels/167_fan_out/throttle_throughput/main.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"sync"
+	"time"
+)
+
+func main() {
+	c1 := make(chan int)
+	c2 := make(chan int)
+
+	go populate(c1)
+
+	go fanOutIn(c1, c2)
+
+	for v := range c2 {
+		fmt.Println(v)
+	}
+
+	fmt.Println("about to exit")
+}
+
+func populate(c chan int) {
+	for i := 0; i < 100; i++ {
+		c <- i
+	}
+	close(c)
+}
+
+func fanOutIn(c1, c2 chan int) {
+	var wg sync.WaitGroup
+	const goroutines = 10
+	wg.Add(goroutines)
+
+	/*
+	Instead of running each received element from channel in a separate goroutine,
+	we want to limit the number of goroutines that process elements.
+	We run 10 goroutines and inside each one, we read from the channel and run
+	expensive operation
+
+	Try to compare it to the bounded TaskExecutor pattern from Java Concurrency in Practice
+	(we limit both number of tasks and number of threads).
+	*/
+	for i := 0; i < goroutines; i++ {
+		go func() {
+			for v := range c1 {
+				func(v2 int) {
+					c2 <- timeConsumingWork(v2)
+				}(v)
+			}
+			wg.Done()
+		}()
+	}
+	wg.Wait()
+	close(c2)
+}
+
+func timeConsumingWork(n int) int {
+	time.Sleep(time.Microsecond * time.Duration(rand.Intn(500)))
+	return n + rand.Intn(1000)
+}
